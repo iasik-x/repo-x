2109cdaf518e316e9809eb53de3a385176c78bfb
kat-iasik <ismail.asik@katmekatronik.com.tr>
2023-01-16 14:39:16 +0300

    Create new project

---
+++ nbproject/Makefile-variables.mk
@@ -0,0 +1,10 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=mplab_project.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/mplab_project.X.production.hex
